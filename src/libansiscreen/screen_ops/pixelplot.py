64a6b7184cf101d332f6c14adfac432b24dc8f09
Gretchen Maculo <gretchynm@gmail.com>
2025-12-23 17:44:53 -0600

    pixelplot comment

--- src/libansiscreen/screen_ops/pixelplot.py
+++ src/libansiscreen/screen_ops/pixelplot.py
@@ -1,9 +1,21 @@
 from __future__ import annotations
-
 from typing import Optional, Tuple
-
 from libansiscreen.screen import Screen
 from libansiscreen.cell import Cell
+from libansiscreen.color.rgb import Color
+from libansiscreen.screen_ops import glyph_defs as G
 
+# simulated graphic framebuffer
+# using the "█", "▀" "▄", " " characters.
+# brighter color is solid.
+# same bright color is solid fg color. same dim color is ' ' bg color
+# if a color/char is set, set both fg/bg intelligently
 
-def pixelplot(self, x, y, color):
+def pixelplot(screen, x, y, color):
+    c=screen.get_cell(x, y/2)
+    if c:
+        if y%2: #bottom block
+            pass
+        else:   #top block
+            pass
+        screen.set_cell(x,y/2, c)
