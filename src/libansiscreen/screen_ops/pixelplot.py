7fe4f1dba9ed3de91eaa752d033a64579d920e2b
Gretchen Maculo <gretchynm@gmail.com>
2025-12-23 17:07:14 -0600

    prototype pixelplot

---
+++ src/libansiscreen/screen_ops/pixelplot.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from typing import Optional, Tuple
+
+from libansiscreen.screen import Screen
+from libansiscreen.cell import Cell
+
+
+def pixelplot(self, x, y, color):
